76c9e45d10510164b7ac975d246d2b57929e413e
amzarzaidi <aamzar.zaidi@gmail.com>
2025-03-24 15:49:45 +0100

    change the re_path to path in both urls.py files

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -1,11 +1,11 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.views.generic import RedirectView
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = [
-    re_path(r'^$', RedirectView.as_view(url='/game/', permanent=False)),
-    path('game/', include('game.urls', namespace='game')),
+    path('', RedirectView.as_view(url='/game/', permanent=False)),
+    path('game/', include(('game.urls', 'game'))),
     path('admin/', admin.site.urls),
 ]

f9bea55f587953386f745879d5eea0fcc16dfcee
amzarzaidi <aamzar.zaidi@gmail.com>
2025-03-31 16:31:25 +0200

    modify the settings to include channels for websocket. change the consumers.py

--- game/consumers.py
+++ game/consumers.py
@@ -1,46 +1,37 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asgiref.sync import async_to_sync
-from django.utils import timezone
 
-
-class ChatConsumer(AsyncWebsocketConsumer):
+class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope['user']
-        self.id = self.scope['url_route']['kwargs']['course_id']
-        self.room_group_name = f'chat_{self.id}'
-        # join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-        # accept connection
+        self.game_id = self.scope['url_route']['kwargs']['game_id']
+        self.room_group_name = f"game_{self.game_id}"
+
+        # Add this connection to the group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
         await self.accept()
 
     async def disconnect(self, close_code):
-        # leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
 
-    # receive message from WebSocket
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        now = timezone.now()
-        # send message to room group
+        data = json.loads(text_data)
+        move = data["move"]
+        player = data["player"]
+
+        # Broadcast the move to all players in the game
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'chat_message',
-                'message': message,
-                'user': self.user.username,
-                'datetime': now.isoformat(),
+                "type": "game_move",
+                "move": move,
+                "player": player,
             }
         )
 
-    # receive message from room group
-    async def chat_message(self, event):
-        # send message to WebSocket
-        await self.send(text_data=json.dumps(event))
+    async def game_move(self, event):
+        await self.send(text_data=json.dumps({
+            "move": event["move"],
+            "player": event["player"],
+        }))

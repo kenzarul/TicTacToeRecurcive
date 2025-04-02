cc6a58cfc8bdeab08406e09e9b0c07f889b6a2c6
amzarzaidi <aamzar.zaidi@gmail.com>
2025-04-02 10:13:38 +0200

    add the routing.py and remake consumers

---
+++ tictactoe/routing.py
@@ -0,0 +1,11 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import game.routing
+
+application = ProtocolTypeRouter({
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            game.routing.websocket_urlpatterns
+        )
+    ),
+})

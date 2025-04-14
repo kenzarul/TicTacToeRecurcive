984f203529acdeecdb9a9b761ae275e67e32fe42
amzarzaidi <aamzar.zaidi@gmail.com>
2025-04-14 11:18:24 +0200

    new test concerning index.html

--- tictactoe/asgi.py
+++ tictactoe/asgi.py
@@ -1,14 +1,13 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 import game.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tictactoe.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tic_tac_toe.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(game.routing.websocket_urlpatterns)
-    ),
+    'http':get_asgi_application(),
+    'websocket': URLRouter(
+        game.routing.websocket_urlpatterns
+    )
 })

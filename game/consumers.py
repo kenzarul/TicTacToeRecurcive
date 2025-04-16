7b1235b85eedba36ef0a121b4762d50187ceca85
amzarzaidi <aamzar.zaidi@gmail.com>
2025-04-16 09:40:53 +0200

    change the index to test_index (still in development). the index file causing error in the deployment

--- game/consumers.py
+++ game/consumers.py
@@ -12,7 +12,6 @@ class GameConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
